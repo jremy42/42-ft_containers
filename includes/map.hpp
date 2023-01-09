ab1a410fa72ab62ea534d51c7021ded3db22e2d9
jonathan <jonathan.prudent13@gmail.com>
2023-01-09 21:15:32 +0100

    [fix const]

--- includes/map.hpp
+++ includes/map.hpp
@@ -23,13 +23,13 @@ namespace ft{
 		typedef Key key_type;
 		typedef T mapped_type;
 		typedef std::pair<const key_type, mapped_type> value_type;
-		typedef ft::rbtree<ft::pair<const Key, T>, Compare, std::allocator<ft::Node<value_type> > > tree_type;
+		typedef	value_type& reference;
+		typedef const value_type& const_reference;
+		typedef ft::rbtree<value_type, Compare, std::allocator<ft::Node<value_type> > > tree_type;
 		typedef Compare key_compare;
 		//value_compare is a binary predicate that takes two element keys as arguments and returns a bool.
 		//typedef typename tree_type::key_compare value_compare;
 		typedef Allocator allocator_type;
-		typedef typename allocator_type::reference reference;
-		typedef typename allocator_type::const_reference const_reference;
 		typedef typename allocator_type::pointer pointer;
 		typedef typename allocator_type::const_pointer const_pointer;
 		typedef typename allocator_type::difference_type difference_type;
